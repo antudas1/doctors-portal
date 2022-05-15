307c0cfbb25b709e48b8d79fa8b9eaa1e69ecd3b
antudas1 <souravdasantu@gmail.com>
2022-05-15 22:42:52 +0600

    backend added

--- src/Components/Page/About/Apointment/AbailableApointments.js
+++ src/Components/Page/About/Apointment/AbailableApointments.js
@@ -9,7 +9,7 @@ const [services,setServices]= useState([])
 const [treatment,setTreaatment]= useState(null);
 
     useEffect(()=>{
-        fetch ('fake.json')
+        fetch ('http://localhost:5000/service')
     .then( res=>res.json())
     .then (data=>setServices(data))
     },[])
@@ -18,7 +18,7 @@ const [treatment,setTreaatment]= useState(null);
             <h1 className=' text-2xl text-primary'>Available Appointment {format(date,'pp')}</h1>
          <div  className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
          {
-              services.map(service=><AbailableApointment key={service.id} service={service} setTreaatment={setTreaatment}></AbailableApointment>)
+              services.map(service=><AbailableApointment key={service._id} service={service} setTreaatment={setTreaatment}></AbailableApointment>)
           }
          </div>
           {treatment&& <BookingModal data={date} treatment={treatment} setTreaatment={setTreaatment}></BookingModal>}
